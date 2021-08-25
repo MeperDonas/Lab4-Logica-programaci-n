2b74caa144e598237041a9d2d1fa8a318192c97b
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2021-08-25 15:18:21 -0500

    Add files via upload

---
+++ Ejercicio 2.1.1.py
@@ -0,0 +1,18 @@
+ #! Ejercico 2.1 
+ 
+num=float(input("Ingrese numero entero: "))
+
+
+operacion_1=(num*num)
+operacion_2=(num**3)
+
+
+
+print("El resultado al cuadrado es: ", operacion_1)
+print("El resultado al cubo es:", operacion_2)
+
+
+
+
+
+
